8217cfb79f2bf75facaafe2fb6b9bd9fec6873db
welikesingh <welikesingh@gmail.com>
2026-09-02 00:36:17 -0500

    Intermediate-Loop-Control.py, Real-World-Loop-Challenges

---
+++ 1_Python/Intermediate-Loop-Control.py
@@ -0,0 +1,127 @@
+# Intermediate Loops & Loop Control
+
+print("Print numbers from 1–100 but skip numbers divisible by 5 using continue.")
+for i in range(1, 101):
+    if i % 5 == 0:
+        continue
+    print(i)
+
+
+
+print("Iterate from 1–100 and stop when you encounter the first number divisible by both 7 and 11.")
+for i in range(1, 101):
+    if (i % 7 == 0) and (i % 11 == 0):
+        print("First number divisible by both 7 and 11 is: ", i)
+        break
+
+
+
+print("Search for a user-provided number inside a list. Use for-else to print:")
+list4=[1,2,3,4,5,6,7,8,9]
+user_input = int(input("Enter a number to search in the list: "))
+print("Searching for number: ", user_input, "in the list: ", list4)
+for num in list4:
+    if num == user_input:
+        print("Number Found")
+        break
+else:
+    print("Number Not Found")
+
+
+
+print("use enumerate() to display indices and values:")
+names = ["Aman", "Ravi", "Sudhanshu", "Priya", "Anjali"]
+list(enumerate(names,start=1))
+
+
+
+print("""Print the following pattern:
+*
+**
+***
+****
+*****""")
+print("---------Printing pattern:---------")
+for i in range(1, 6):
+    print('*' * i)
+
+
+
+print("""Print the following pattern:
+*****
+****
+***
+**
+*""")
+print("------Printing pattern:------")
+for i in range(1, 6):
+    print('*' * (6-i))
+
+
+
+
+print("Generate multiplication tables from 1 to 10 using nested loops.")
+for i in range(1, 11):
+    print(f"Multiplication Table for {i}:")
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i*j}")
+    print()  
+
+
+
+print("Find all numbers between 1 and 200 divisible by both 3 and 5.")
+for i in range(1, 201):
+    if (i % 3 == 0) and (i % 5 == 0):
+        print(i)    
+
+
+
+print("Given a list containing duplicate elements, \n" 
+"Create another list containing only unique elements without using set().")
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5]
+unique_list = []
+for item in original_list:
+    if item not in unique_list:
+        unique_list.append(item)
+print("Original List:", original_list)
+print("Unique List:", unique_list)
+
+
+
+
+numbers = [10, -4, 8, -2, 0, 15, -9, 21]
+print(" Count :", len(numbers))
+print("Positive numbers:", [x for x in numbers if x > 0])
+print("Negative numbers:", [x for x in numbers if x < 0])
+zeros = [x for x in numbers if x == 0]
+print("Count of Zeros:", len(zeros))    
+
+
+
+
+print("Write a program to determine whether a number is prime using a loop.")
+n = int(input("Enter a number: "))
+#most efficient and common approach is to check for divisors up to the square root of the number
+def is_prime_number(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+if is_prime_number(n):
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is not a prime number.")
+
+
+
+
+
+
+print("Print all prime numbers between 1 and 100.")
+prime_numbers = []
+for num in range(1, 101):
+    if is_prime_number(num):
+        prime_numbers.append(num)
+print("Prime numbers between 1 and 100:", prime_numbers)    
---
+++ 1_Python/Real-World-Loop-Challenges.py
@@ -0,0 +1,176 @@
+# calculate total transaction value without using sum().
+# find the highest and lowest transaction without max() and min().
+transactions = [1200, 450, 800, 1500, 2300, 700, 100]
+sum=0
+highest=transactions[0]
+lowest=transactions[0]
+
+for elem in transactions:
+    # sum
+    sum=sum+elem
+    # highest
+    if elem > highest:
+        highest=elem
+    if elem < lowest:
+        lowest= elem
+
+print(f"Sum: {sum}", f"Highest: {highest}", f"Lowest: {lowest}", sep='\n')
+
+print("------------------------------------------------------------")
+
+
+# Find the average temperature.
+temperatures = [32, 35, 28, 40, 38, 31, 42]
+element_cnt = len(temperatures)
+sum=0
+for i in temperatures:
+    sum=sum+i
+print("average temperature: ", sum/element_cnt)   
+print("------------------------------------------------------------")
+
+
+#Given student marks
+marks = [78, 92, 45, 67, 88, 53, 99]
+# count how many students scored
+# 90+
+# 75–89
+# 50–74
+# below 50
+cnt_90Above=0
+cnt_75_89 =0
+cnt_50_74 =0
+cnt_below_50 =0
+for i in marks:
+  if i > 90:
+    cnt_90Above= cnt_90Above + 1
+  if ( i > 75 & i < 89) :
+    cnt_75_89 = cnt_75_89 + 1
+  if ( i > 50 & i < 74) :
+    cnt_50_74 = cnt_50_74 + 1  
+  if ( i < 50) :
+    cnt_below_50 = cnt_below_50 + 1    
+
+print(f"90+ :{cnt_90Above}")  
+print(f"75–89 :{cnt_75_89}")  
+print(f"50–74 :{cnt_50_74}")  
+print(f"below 50 :{cnt_below_50}")  
+
+
+print("------------------------------------------------------------")
+
+# Create a simple login system with a maximum of 3 password attempts.
+entered_user= input("Please enter user: ")
+entered_password= input("Enter password: ")
+list_users = [
+            {"user":"user1", "password":"password1"},
+            {"user":"user2", "password":"password2"},
+            {"user":"user3", "password":"password3"},
+            {"user":"user4", "password":"password4"}
+          ]
+l_success='NO'
+for i in range(2):
+  for lst in list_users:
+    if (lst['user'] == entered_user) & (lst['password'] == entered_password):
+      l_success='YES'
+      print("You are successfully loggedin !!")
+      break
+  if l_success != 'YES':
+    entered_user= input("Entered credential incorrect, Please re enter user: ")
+    entered_password= input("Entered credential incorrect, Please re enter password: ")
+  else:
+    break
+
+
+print("------------------------------------------------------------")
+
+products = {
+            "Laptop": 55000,
+            "Phone": 30000,
+            "Headphones": 2000,
+            "Mouse": 700,
+            "Keyboard": 1500
+            }
+
+print("print only products costing more than ₹2,000.")
+for key, value in products.items():
+    if value > 2000:
+        print("Product: ",key)
+
+
+print("------------------------------------------------------------")
+
+print("Accept 10 numbers from the user and store them in a list using a loop.")
+list2=[]
+for i in range(10):
+   inum= int(input("Enter a number: "))
+   list2.append(inum)
+
+print(list2)
+
+print("------------------------------------------------------------")
+
+print("Count the frequency of every character in a string without using Counter.")
+
+str='banana'
+set2=set(str)
+#print(set2)
+for i in list(set2):
+    print(i,"-->",str.count(i))
+
+
+
+print("------------------------------------------------------------")
+print("Find the second-largest number in a list without using sort().")
+list3=[2,4,5,6,6,8,8,3]
+#list3.sort()
+#print(list3[1])
+for i in list3:
+    if i > max(list3):
+        list3.remove(i)
+print(max(list3))
+print("------------------------------------------------------------")
+
+print("Check whether a string is a palindrome using loops.")
+s='madam'
+i=0
+j =len(s) - 1  # last index
+is_palindrome = True  
+
+while i < j:
+    if s[i] != s[j]:  
+        is_palindrome = False
+        break
+    i += 1
+    j -= 1
+
+if is_palindrome:
+    print("Yes") 
+else:
+    print("No") 
+print("------------------------------------------------------------")
+print("Create a basic ATM simulation where a user can repeatedly")
+print("Create a basic ATM simulation where a user can repeatedly")
+
+def options_display():
+    print("Please select option: ")
+    print("Enter 1: To check Balance ")
+    print("Enter 2: To Deposit Money ")
+    print("Enter 3: To Withdraw money ")
+    print("Enter exit: To exit from System ")
+
+options_display()
+ip=input("Choose option ")
+print("User selected option: ", ip)
+while ip.upper() != 'EXIT':
+    if (ip == '1'): 
+      print("***Checking Balance processing***")
+    elif (ip == '2') :
+      print("***Deposit Money processing***")
+    elif (ip == '3') :
+      print("***Withdraw money processing***")
+    else:
+      print("***Invalid Option, Please retry***")
+    options_display()
+    ip=input("Choose option ")
+    print("User selected option: ", ip)
+print("------------------------------------------------------------")
